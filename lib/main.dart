6603c8a34de5bd01128d470a33e58b914a45ec65
niksoni2910 <nikhilsoni2910@gmail.com>
2023-09-15 14:16:25 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:health_app/quiz.dart';
+
+void main() {
+  runApp(QuizApp());
+}
+
+class QuizApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: QuizScreen(),
+    );
+  }
+}
+
+
