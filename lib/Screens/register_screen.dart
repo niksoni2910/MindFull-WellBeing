ca054ef2971f09703c63a63e18959fa8c4f4b940
niksoni2910 <nikhilsoni2910@gmail.com>
2023-09-20 02:38:48 +0530

    workout page done

--- lib/Screens/register_screen.dart
+++ lib/Screens/register_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:health_app/bottom_navigator.dart';
 import 'package:health_app/constants/custom_button.dart';
 import 'package:health_app/constants/custom_dropdown.dart';
 import 'package:health_app/constants/statesandcity.dart';
@@ -22,7 +23,6 @@ class _RegistrationPageState extends State<RegistrationPage> {
 
   String selectedState = '';
   String selectedCity = '';
-  
 
   @override
   Widget build(BuildContext context) {
@@ -30,59 +30,73 @@ class _RegistrationPageState extends State<RegistrationPage> {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          decoration:BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/back.jpg"),
-              fit: BoxFit.cover,
-            )),
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage("assets/back.jpg"),
+            fit: BoxFit.cover,
+          )),
           child: Center(
             child: SingleChildScrollView(
               scrollDirection: Axis.vertical,
-              padding: EdgeInsets.only(top: size.height * 0.015,bottom: size.height * 0.015,left: size.width * 0.04,right: size.width * 0.04),
+              padding: EdgeInsets.only(
+                  top: size.height * 0.015,
+                  bottom: size.height * 0.015,
+                  left: size.width * 0.04,
+                  right: size.width * 0.04),
               child: Container(
                 alignment: Alignment.center,
-                  padding:  EdgeInsets.symmetric(horizontal: size.width * 0.03, vertical: size.height * 0.025),
-                    width: size.width * 0.9,
-                    height: size.height * 0.87,
-                    decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20.0),
-              color: Colors.white,
-                    ),
+                padding: EdgeInsets.symmetric(
+                    horizontal: size.width * 0.03,
+                    vertical: size.height * 0.025),
+                width: size.width * 0.9,
+                height: size.height * 0.87,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: Colors.white,
+                ),
                 child: Form(
                   key: _formKey,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       Expanded(
-                    flex: 1,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text.rich(
-                          TextSpan(
-                            style: GoogleFonts.inter(
-                              fontSize: 24.0,
-                              color: const Color(0xFF21899C),
-                              letterSpacing: 2.0,
+                        flex: 1,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text.rich(
+                              TextSpan(
+                                style: GoogleFonts.inter(
+                                  fontSize: 24.0,
+                                  color: const Color(0xFF21899C),
+                                  letterSpacing: 2.0,
+                                ),
+                                children: const [
+                                  TextSpan(
+                                      text: 'REGI',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w800)),
+                                  TextSpan(
+                                      text: 'STER',
+                                      style: TextStyle(
+                                          color: Color(0xFFFE9879),
+                                          fontWeight: FontWeight.w800)),
+                                ],
+                              ),
+                              textAlign: TextAlign.center,
                             ),
-                            children: const [
-                              TextSpan(text: 'REGI', style: TextStyle(fontWeight: FontWeight.w800)),
-                              TextSpan(text: 'STER', style: TextStyle(color: Color(0xFFFE9879), fontWeight: FontWeight.w800)),
-                            ],
-                          ),
-                          textAlign: TextAlign.center,
+                          ],
                         ),
-                      ],
-                    ),
-                    ),
+                      ),
                       SizedBox(
                         height: size.height / 14,
                         child: CustomTextField(
                           controller: nameController,
                           hintText: 'Enter Your Name',
                           pIcon: Icon(Icons.person,
-                              color: const Color(0xFF151624).withOpacity(0.5), size: 16),
+                              color: const Color(0xFF151624).withOpacity(0.5),
+                              size: 16),
                           validator: (value) {
                             if (value == null || value.isEmpty) {
                               return 'Please enter your name';
@@ -100,7 +114,8 @@ class _RegistrationPageState extends State<RegistrationPage> {
                           controller: ageController,
                           hintText: 'Enter Your Age',
                           pIcon: Icon(Icons.calendar_today,
-                              color: const Color(0xFF151624).withOpacity(0.5), size: 16),
+                              color: const Color(0xFF151624).withOpacity(0.5),
+                              size: 16),
                           validator: (value) {
                             if (value == null || value.isEmpty) {
                               return 'Please enter your age';
@@ -118,7 +133,8 @@ class _RegistrationPageState extends State<RegistrationPage> {
                           controller: emailController,
                           hintText: 'Enter Your Email',
                           pIcon: Icon(Icons.email,
-                              color: const Color(0xFF151624).withOpacity(0.5), size: 16),
+                              color: const Color(0xFF151624).withOpacity(0.5),
+                              size: 16),
                           validator: (value) {
                             if (value == null || value.isEmpty) {
                               return 'Please enter your email';
@@ -134,7 +150,8 @@ class _RegistrationPageState extends State<RegistrationPage> {
                       SizedBox(
                         height: size.height / 14,
                         child: CustomDropdownFormField<String?>(
-                          selectedValue: selectedState.isNotEmpty ? selectedState : null,
+                          selectedValue:
+                              selectedState.isNotEmpty ? selectedState : null,
                           items: StatesAndCiti.states,
                           onChanged: (String? newValue) {
                             setState(() {
@@ -155,7 +172,8 @@ class _RegistrationPageState extends State<RegistrationPage> {
                       SizedBox(
                         height: size.height / 14,
                         child: CustomDropdownFormField<String?>(
-                          selectedValue: selectedCity.isNotEmpty ? selectedCity : null,
+                          selectedValue:
+                              selectedCity.isNotEmpty ? selectedCity : null,
                           items: StatesAndCiti.cities[selectedState] ?? [],
                           onChanged: (String? newValue) {
                             setState(() {
@@ -178,7 +196,8 @@ class _RegistrationPageState extends State<RegistrationPage> {
                           controller: passwordController,
                           hintText: 'Enter Your Password',
                           pIcon: Icon(Icons.lock_outline_rounded,
-                              color: const Color(0xFF151624).withOpacity(0.5), size: 16),
+                              color: const Color(0xFF151624).withOpacity(0.5),
+                              size: 16),
                           validator: (value) {
                             if (value == null || value.isEmpty) {
                               return 'Please enter your password';
@@ -197,7 +216,8 @@ class _RegistrationPageState extends State<RegistrationPage> {
                           controller: confirmPasswordController,
                           hintText: 'Confirm Your Password',
                           pIcon: Icon(Icons.lock_outline_rounded,
-                              color: const Color(0xFF151624).withOpacity(0.5), size: 16),
+                              color: const Color(0xFF151624).withOpacity(0.5),
+                              size: 16),
                           validator: (value) {
                             if (value == null || value.isEmpty) {
                               return 'Please confirm your password';
@@ -224,14 +244,19 @@ class _RegistrationPageState extends State<RegistrationPage> {
                               String state = selectedState;
                               String city = selectedCity;
                               String password = passwordController.text;
-              
+
                               // Implement registration logic here
                               // For example, send data to your backend or perform local registration
-              
+
                               // Reset form after registration
                               _formKey.currentState!.reset();
                               selectedState = '';
                               selectedCity = '';
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => UserBottomNav(),
+                                  ));
                             }
                           })
                     ],
@@ -245,5 +270,3 @@ class _RegistrationPageState extends State<RegistrationPage> {
     );
   }
 }
-
-
